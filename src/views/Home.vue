f1ce6f44ef473cc192336845d50bcadfe8c25961
JasonH <jasonh3308@gmail.com>
2018-11-30 20:29:29 -0800

    test facebook messenger
    try

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="home">
-    <div id="fb-root"></div>
-    <!-- Your customer chat code -->
-    <div class="fb-customerchat"
-         attribution="setup_tool"
-         page_id="516083218869346"
-         logged_in_greeting="Hi! How can we help you?"
-         logged_out_greeting="Hi! How can we help you?">
-    </div>
   </div>
 </template>
 
@@ -17,9 +9,6 @@
     name: 'home',
     components: {},
     mounted() {
-      let recaptchaScript = document.createElement('script')
-      recaptchaScript.setAttribute('src', 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js')
-      document.body.appendChild(recaptchaScript)
     },
   }
 </script>
