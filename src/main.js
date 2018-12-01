fcfbdbb753ee7a942b792b957e44ecd96bee557b
JasonH <jasonh3308@gmail.com>
2018-11-30 19:57:25 -0800

    test facebook messenger

--- src/main.js
+++ src/main.js
@@ -9,14 +9,4 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  created() {
-    (function(d, s, id) {
-      console.log('loading facebook sdk')
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'))
-  },
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div id="fb-root"></div>
     <!-- Your customer chat code -->
     <div class="fb-customerchat"
-         attribution=setup_tool
+         attribution="setup_tool"
          page_id="516083218869346"
          logged_in_greeting="Hi! How can we help you?"
          logged_out_greeting="Hi! How can we help you?">
@@ -13,14 +13,13 @@
 
 <script>
 
-export default {
-  name: 'home',
-  components: {
-  },
-  mounted() {
-    window.extAsyncInit = function () {
-      console.log('haha')
-    }
-  },
-}
+  export default {
+    name: 'home',
+    components: {},
+    mounted() {
+      let recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js')
+      document.body.appendChild(recaptchaScript)
+    },
+  }
 </script>
